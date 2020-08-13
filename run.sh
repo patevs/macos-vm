c415ba4d1d68b5b5f1f8ff13f3c739fcffe04279
Patrick Evans <ppevans11@gmail.com>
2020-08-13 14:57:56 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# ------ #
+# run.sh #
+# ------ #
+
+echo '' # new line
+
+# --------------------------------------------------------------------------------------------------- #
+
+# Check environment
+
+# Ensure wget command exists
+if ! [ -x "$(command -v wget)" ]; then
+  echo 'Error: wget is not installed. Exiting...' >&2
+  exit 1
+fi
+
+# --------------------------------------------------------------------------------------------------- #
+
+# Download installer script
+wget https://raw.githubusercontent.com/myspaghetti/macos-virtualbox/master/macos-guest-virtualbox.sh
+
+# Run installer script
+bash ./macos-guest-virtualbos.sh
+
+# --------------------------------------------------------------------------------------------------- #
+
+# EOF #
