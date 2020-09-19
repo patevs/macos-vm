6cb9cdf41b9038d5e2f447e7338fe042e70f31af
Patrick Evans <ppevans11@gmail.com>
2020-09-19 17:37:22 +1200

    :sparkles: Updated deps.sh

--- deps.sh
+++ deps.sh
@@ -13,8 +13,35 @@ echo '' # new line
 
 # TODO: Ensure brew install successfully
 
-# Install iTerm2
+# iTerm2
 brew cask install iterm2
+
+# NodeJS
+# brew install node  # Latest
+brew install node@12 # LTS
+
+# TODO: Ensure node installed successfully
+
+# Updated NPM
+npm install --global npm
+
+# NPM Global Packages
+npm install --global \
+add-gitignore \
+check-it-out \
+diff-so-fancy \
+g3l \
+gtop \
+gitmoji-cli \
+npm-check \
+npm-check-updates \
+ntl \
+licensed \
+neovim \
+open-cli \
+emus \
+typesync \
+react-native-cli
 
 # --------------------------------------------------------------------------------------------------- #
 
