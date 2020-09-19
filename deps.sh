61d56a04e7e07fbe3f55d597aad32a217b5eb8b1
Patrick Evans <ppevans11@gmail.com>
2020-09-19 17:32:55 +1200

    :sparkles: Updated deps.sh

--- deps.sh
+++ deps.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# ------ #
+# run.sh #
+# ------ #
+
+echo '' # new line
+
+# --------------------------------------------------------------------------------------------------- #
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install iTerm2
+brew cask install iterm2
+
+# --------------------------------------------------------------------------------------------------- #
+
+# EOF #
