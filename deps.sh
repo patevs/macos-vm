231b5cb772bd85ea37b5df2b01afd2a040d7d9ff
Patrick Evans <ppevans11@gmail.com>
2020-09-19 17:33:19 +1200

    :bulb: Added comments to deps.sh

--- deps.sh
+++ deps.sh
@@ -11,6 +11,8 @@ echo '' # new line
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# TODO: Ensure brew install successfully
+
 # Install iTerm2
 brew cask install iterm2
 
